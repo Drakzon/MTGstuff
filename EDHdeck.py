d6f4ef5e7d2f87696712d4c7845f8db33f6d80dc
Drakzon <danslake@gmail.com>
2017-03-09 00:55:43 +0000

    Attempted to add cost to complete estimate
    Very inefficient, add in price dictionary to stop it checking the price of the same card multiple times
    
    probably rewrite whole thing with a deck class

--- EDHdeck.py
+++ EDHdeck.py
@@ -6,6 +6,11 @@ import numpy as np
 import pickle
 import csv
 import os
+import unicodedata
+
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
         
 def GrabEDHREC():   
     'Function goes through and grabs the average deck for each commander on EDHrec'
@@ -58,9 +63,29 @@ def GrabDecklist(soup):
     DecklistXML = soup.find('div',class_="container well").text
     temp = '1'
     SplitXML = re.split('1 |2 |3 |4 |5 |6 |7 |8 |9 |0', DecklistXML)
+    SplitXML2 =''.join(i for i in SplitXML if not i.isdigit())
+#    SplitXML3 = remove_accents(SplitXML2)
     Numbers = re.findall(r'\d+', temp + DecklistXML)
-    Decklist = [list(x) for x in zip(Numbers, SplitXML)]
+    Decklist = [list(x) for x in zip(Numbers, SplitXML2)]
     return(Decklist)
+    
+def GetPriceManaleak(card):
+    "Gets a price from manaleak"
+    
+    sauce = urllib.request.urlopen('http://www.manaleak.com/magic-the-gathering/advanced_search_result.php?keywords=' + card.replace(" ", "+")).read()
+    soup = bs.BeautifulSoup(sauce,'lxml')
+
+    cardPrices = soup.find('', class_="content t_view")
+    priceTemp = cardPrices.find_all('div',class_="product_price_wrapper")
+    pricelist = []
+
+    for prices in priceTemp:
+        PricewCurr = prices.find('span').text
+        PricenoCurr = PricewCurr.split('£')[1]
+        pricelist.append(float(PricenoCurr))
+    return(min(pricelist))
+
+    
     
 #class Deck:
 #    "A Class to hold deck info"
@@ -109,29 +134,37 @@ for key in decks.keys():
         GeneralList.append(key)
 
 score = {}
+score2 = {}
 OwnedCards = {}
 MissingCards = {}
 TotalCards = {}
-
-print('Scoring')
+#
+print('Pricing')
     
 for EDHDeck in GeneralList: 
     OwnedCards[EDHDeck] = 0
     TotalCards[EDHDeck] = 0
     MissingCards[EDHDeck] = []
     score[EDHDeck] = 0
-    for card in Decks[EDHDeck]:
+    pricetocomplete = 0
+    print(EDHDeck)
+    for card in decks[EDHDeck]:
+        card[1] = remove_accents(''.join(i for i in card[1] if not i.isdigit()))
         TotalCards[EDHDeck] += 1
-        if card[1] in collection:
+        if card in collection:
 #           print('Own ' + card[1])
            OwnedCards[EDHDeck] += 1
         else:
-#            print('Missing ' + card[1])
+            print('Missing ' + card[1])
             MissingCards[EDHDeck].append(card)
-    score[Deck] = OwnedCards[EDHDeck]/TotalCards[EDHDeck]
+            pricetocomplete += GetPriceManaleak(card[1])
+    score[EDHDeck] = OwnedCards[EDHDeck]/TotalCards[EDHDeck]
+    score2[EDHDeck] = pricetocomplete
+    print('Deck: ' + EDHDeck + 'would cost around £' + str(round(pricetocomplete)) + ' to complete')
 
     
 SortedScores = sorted(score.items(), key=lambda x: x[1], reverse=True)
+SortedPrices = sorted(score.items(), key=lambda x: x[1], reverse=True)
 
 #for A in SortedScores:
 #    print(A)
@@ -142,24 +175,46 @@ with open('Scores.csv','w', newline='') as out:
     for row in SortedScores:
         csv_out.writerow(row)
       
+with open('Prices.csv','w', newline='') as out:
+    csv_out=csv.writer(out)
+    csv_out.writerow(['Deck','Cost to complete'])
+    for row in SortedPrices:
+        csv_out.writerow(row)
+        
 print('Finished')
 
 
-
-##Deck = GeneralList[40]
-#Deck = 'Yidris, Maelstrom Wielder'
+#Deck = GeneralList[40]
+#EDHDeck = 'Yidris, Maelstrom Wielder'
 #
-#for card in Decks[Deck]:
-#    TotalCards[Deck] += 1
+#OwnedCards[EDHDeck] = 0
+#TotalCards[EDHDeck] = 0
+#MissingCards[EDHDeck] = []
+#score[EDHDeck] = 0
+#score2[EDHDeck] = 0
+#for card in decks[EDHDeck]:
+#    TotalCards[EDHDeck] += 1
 #    if card[1] in collection:
-#        print('Own ' + card[1])
-#        OwnedCards[Deck] += 1
+##       print('Own ' + card[1])
+#      OwnedCards[EDHDeck] += 1
 #    else:
-#        print('Missing ' + card[1])
-#        MissingCards[Deck] += -1
+##        print('Missing ' + card[1])
+#        MissingCards[EDHDeck].append(card)
+#        score2[EDHDeck] += GetPriceManaleak(card[1])
+#        print(card[1])
+#score[EDHDeck] = OwnedCards[EDHDeck]/TotalCards[EDHDeck]
 #
-#score[Deck] = TotalCards[Deck] + MissingCards[Deck]
+#
+#print(score[EDHDeck])
+#print(score2[EDHDeck])
 
-#print(score[Deck])
+
+#cardprice = GetPriceManaleak(card)
+
+
+
+
+
+
 
 
